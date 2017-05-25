72430be94deede128be9514ae95065ddae2adc42
Kirstin Hartman <kirihartman8@gmail.com>
2017-05-25 16:52:12 -0700

    continue working on variable scope and forms, adding to styles.css

---
+++ js/scripts.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+  $("#blanks form") .submit(function(event) {
+    var location = $("input#location").val();
+    var famousperson = $("input#famousperson").val();
+    var adjective1 = $("input#adjective1").val();
+    var sound = $("input#sound").val();
+    var animal1 = $("input#animal1").val();
+    var animal2 = $("input#animal2").val();
+    var animal3 = $("input#animal3").val();
+    var verb1 = $("input#verb1").val();
+    var typeofperson = $("input#typeofperson").val();
+    var noun = $("input#noun").val();
+    var verb2 = $("input#verb2").val();
+    var store = $("input#store").val();
+    var raceofpeople = $("input#raceofpeople").val();
+    var alrightterrible = $("input#alrightterrible").val();
+    var famouswoman = $("input#famouswoman").val();
+    var verb3 = $("input#verb3").val();
+    var groupofpeople = $("input#groupofpeople").val();
+    var place = $("input#place").val();
+    var game = $("input#game").val();
+    var setting = $("input#setting").val();
+    var edverb = $("input#edverb").val();
+    var accomplishment = $("input#accomplishment").val();
+    var pluralnoun = $("input#pluralnoun").val();
+    var unsuccessfulathlete = $("input#unsuccessfulathlete").val();
+    var adjective2 = $("input#adjective2").val();
+
+
+
+
+    $(".location").append("blah blah");
+    $(".famousperson").append("blah blah");
+    $(".adjective1").append("blah blah");
+    $(".sound").append("blah blah");
+    $(".animal1").append("blah blah");
+    $(".animal2").append("blah blah");
+    $(".animal3").append("blah blah");
+    $(".verb1").append("blah blah");
+    $(".typeofperson").append("blah blah");
+    $(".noun").append("blah blah");
+    $(".verb2").append("blah blah");
+    $(".store").append("blah blah");
+    $(".raceofpeople").append("blah blah");
+    $(".alright/terrible").append("blah blah");
+    $(".famouswoman").append("blah blah");
+    $(".verb3").append("blah blah");
+    $(".groupofpeople").append("blah blah");
+    $(".place").append("blah blah");
+    $(".game").append("blah blah");
+    $(".setting").append("blah blah");
+    $(".-edverb").append("blah blah");
+    $(".accomplishment").append("blah blah");
+    $(".pluralnoun").append("blah blah");
+    $(".unsuccessfulathlete").append("blah blah");
+    $(".adjective2").append("blah blah");
+
+    $("#story").show();
+
+    event.preventDefault();
+  });
+});
